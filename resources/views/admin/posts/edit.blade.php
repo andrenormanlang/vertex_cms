874b96b908f6e4b503ed1a792fe243848582ccd2
andrenormanlang <andrenormanlang@gmail.com>
2024-11-30 14:27:48 +0100

    pasting rich text instead of only link

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -111,7 +111,7 @@
             function initializeTinyMCE(selector, isDarkMode) {
                 tinymce.init({
                     selector: selector,
-                    plugins: 'link image lists preview code textcolor colorpicker imagetools fontsize paste',
+                    plugins: 'image lists preview code textcolor colorpicker imagetools fontsize paste',
                     toolbar: 'undo redo | fontfamily fontsize | bold italic underline forecolor backcolor | link image | align lineheight checklist bullist numlist | indent outdent | removeformat typography | preview code',
                     menubar: false,
                     branding: false,
